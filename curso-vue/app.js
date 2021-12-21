872abe281f01b21702c771001d02a14b6a3e7530
Francisco Arenas <wook82@gmail.com>
2021-12-21 00:28:36 -0300

    agrego estructura principal del curso

---
+++ curso-vue/app.js
@@ -0,0 +1,11 @@
+new Vue({
+  el: '#app',
+
+  data() {
+    return {
+      name: 'Bitcoin', 
+      img: 'https://img.icons8.com/color/480/000000/bitcoin--v3.png',
+      changePercent: 10
+    }
+  }
+})
