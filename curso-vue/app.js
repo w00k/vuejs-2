389dc04295fbfcbe0ae488b8f44bf3b425eafbfc
Francisco Arenas <farenas@NI-NTB-120.local>
2021-12-21 23:50:04 -0300

    agrego sección donde agramos el two way data binding

--- curso-vue/app.js
+++ curso-vue/app.js
@@ -7,6 +7,7 @@ new Vue({
       symbol: 'BTC',
       img: 'https://img.icons8.com/color/480/000000/bitcoin--v3.png',
       changePercent: 10,
+      value: 0,
       color: 'f4f4f4',
       price: 8400,
       pricesWithDays: [
@@ -24,6 +25,12 @@ new Vue({
   computed: {
     title() {
       return `${this.name} - ${this.symbol}`;
+    },
+    convertedValue() {
+      if(this.value == 0) {
+        return 0;
+      } 
+      return this.value / this.price;
     }
   },
   watch: {
