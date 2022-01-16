9744092b70d96b9f8e6c56a86c4d252174019cfe
Francisco Arenas <wook82@gmail.com>
2022-01-16 14:08:08 -0300

    tarea del modal

--- componentes-vue/app.js
+++ componentes-vue/app.js
@@ -1,18 +1,41 @@
 Vue.component('modal', {
+    props: ['title'],
+
+    methods: {
+        close() {
+            this.$emit('close');
+        }
+    },
+
     template: `
-      <div class="modal-mask">
-        <div class="modal-wrapper">
-          <div class="modal-container">
-            <h3>title</h3>
-            <div>body</div>
-            <footer>
-              <button>Cerrar</button>
-            </footer>
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+          <h3>{{ title }}</h3>
+          <div>
+            <slot name="body"></slot>
           </div>
+          <footer>
+            <button v-on:click="close">Cerrar</button>
+          </footer>
         </div>
-      </div>`
-  })
-  
-  new Vue({
-    el: '#app'
-  })
+      </div>
+    </div>`
+})
+
+new Vue({
+    el: '#app',
+
+    data() {
+        return {
+            title: 'Titulo del modal',
+            showModal: false
+        }
+    },
+
+    methods: {
+        toggleModal() {
+            this.showModal = !this.showModal
+        }
+    }
+})
